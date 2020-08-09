eed7ce9880746e6774bf2c56db742f98d1852370
KRozumniak <kirkterra@gmail.com>
2020-08-09 00:43:06 -0400

    Add Codewars Solution "Simple Fun #1: Seats in Theater"

---
+++ 7kuy/seatsInTheater.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/588417e576933b0ec9000045
+
+function seatsInTheater(nCols, nRows, col, row) {
+    let res;
+    res = (nRows - row) * (nCols - (col - 1));
+    return res;
+}
