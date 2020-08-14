5a2d45424d3f87d126d76495a0956bb07788a0b1
KRozumniak <kirkterra@gmail.com>
2020-08-14 15:52:18 -0400

    Add Codewars Solution "L1: Set Alarm"

---
+++ 8kuy/setAlarm.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/568dcc3c7f12767a62000038
+
+function setAlarm(employed, vacation){
+    if (employed && vacation) return false;
+    else if (employed) return true;
+    else return false;
+}
