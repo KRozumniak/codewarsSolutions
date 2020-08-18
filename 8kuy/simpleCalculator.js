ce9a1ef7ef6b0c49e5d3fe2e45c1177f436b385e
KRozumniak <kirkterra@gmail.com>
2020-08-18 11:34:42 -0400

    Add Codewars Solution "simple calculator"

---
+++ 8kuy/simpleCalculator.js
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/5810085c533d69f4980001cf/
+
+function calculator(a,b,sign){
+    if (typeof a === 'number' && typeof b === 'number') {
+        switch(sign) {
+            case '+': return a + b;
+            case '-': return a - b;
+            case '*': return a * b;
+            case '/': return a / b;
+            default: return 'unknown value';
+        }
+    } else {
+        return 'unknown value';
+    }
+}
