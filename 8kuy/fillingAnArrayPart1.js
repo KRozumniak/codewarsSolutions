745ec14f6f85bc634a10f3694585ef446649823a
KRozumniak <kirkterra@gmail.com>
2020-09-04 15:56:45 -0400

    Add Codewars Solution "Filling an array (part 1)"

---
+++ 8kuy/fillingAnArrayPart1.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/571d42206414b103dc0006a1/
+
+function arr (N) {
+    const arr = [];
+    for (let i = 0; i < N; i++){
+        arr.push(i);
+    }
+    return arr;
+};
+
