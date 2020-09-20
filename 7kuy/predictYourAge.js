eaf25df740d7c12f21079578b500e0e9459ea91d
KRozumniak <kirkterra@gmail.com>
2020-09-20 14:50:18 -0400

    Add Codewars Solution "Predict your age!"

--- 7kuy/predictYourAge.js
+++ 7kuy/predictYourAge.js
@@ -1,9 +1,10 @@
 //https://www.codewars.com/kata/5aff237c578a14752d0035ae/
 
-function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
+function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
     let arr = [age1, age2, age3, age4, age5, age6, age7, age8];
     let multArr = arr.map(el => el * el);
     let res = multArr.reduce((acc, cur) => acc + cur);
     res = Math.sqrt(res);
     res /= 2;
     return Math.floor(res);
+}
