06181222f71955b01d9906b96d45e4a2924f4fd9
KRozumniak <kirkterra@gmail.com>
2020-10-20 19:32:51 -0400

    Add Codewars Solution

---
+++ 8kuy/JennysSecretMessage.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/55225023e1be1ec8bc000390/
+
+function greet(name){
+    let str = "Hello, " + name + "!";
+    if (name === "Johnny") {
+        return "Hello, my love!";
+    } else {
+        return str;
+    }
+}
