7c1492e85245c5984d8424a36153debe1911c9b4
KRozumniak <kirkterra@gmail.com>
2020-10-20 23:59:32 -0400

    Add Codewars Solution

---
+++ 6kuy/UniqueInOrder.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/54e6533c92449cc251001667/
+
+const uniqueInOrder = iterable => {
+    const arr = [];
+    for (let i = 0; i < iterable.length; i++) {
+        if (iterable[i] === iterable[i + 1]) continue;
+        arr.push(iterable[i])
+    }
+    return arr;
+}
