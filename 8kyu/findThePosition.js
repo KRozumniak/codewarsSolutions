203476abf7e091900b52e9606d1f972524176cdc
KRozumniak <kirkterra@gmail.com>
2020-10-25 20:32:22 -0400

    Add Codewars Solution

---
+++ 8kyu/findThePosition.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5808e2006b65bff35500008f/
+
+function position(letter){
+    let alphabet = '0abcdefghijklmnopqrstuvwxyz';
+    let res = alphabet.indexOf(letter);
+    return `Position of alphabet: ${res}`;
+}
