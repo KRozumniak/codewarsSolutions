e7d795e0ef0d4889286b85eaf0060d4adc1b04b8
KRozumniak <kirkterra@gmail.com>
2021-03-26 00:02:33 -0400

    Add Codewars Solution

---
+++ 7kyu/anagramDetection.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/529eef7a9194e0cbc1000255/
+
+// const isAnagram = (test, original) => {
+//   const w1 = test.toLowerCase().split('').sort().join('');
+//   const w2 = original.toLowerCase().split('').sort().join('');
+//   return w1 === w2;
+// }
