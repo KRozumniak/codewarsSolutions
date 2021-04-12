d83bc8a61ec3db48af0a8bff462a0dad8851f3c6
KRozumniak <kirkterra@gmail.com>
2021-04-12 00:09:01 -0400

    Kata solution added

---
+++ 8kyu/grasshopperCreateTheRooms.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/56a29b237e9e997ff2000048/
+
+const rooms = {
+  first: {
+    name: 'first',
+    description: 'desc',
+    completed: false,
+  },
+  second: {
+    name: 'second',
+    description: 'desc',
+    completed: false,
+  },
+  third: {
+    name: 'third',
+    description: 'desc',
+    completed: false,
+  }
+}
